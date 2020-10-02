ddee82f07b73efa7c7949614d23b7bc4e0664961
Mariano Lopez <marianolop22@yahoo.com.ar>
2020-10-02 11:23:26 -0300

    fin seccion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log("hola mundo");
+
+
+//clon de objetos
+const persona = {
+    nombre: "Mariano",
+    apellido : "Lopez",
+    edad: "42"
+};
+const persona2 = {...persona}; //Esto hace copia del objeto
+persona2.nombre = "pepe";
+
+//concatenar arrays
+const miArray = [1,2,3,4];
+
+const miArray2 = [...miArray,5];
+
+console.table(miArray2);
+
+
+
+
+
+
